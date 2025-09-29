9490084804f91317f8073ba6eeb2479cc737141f
ABISHEK ANTONY <abishek8673@gmail.com>
2025-09-29 11:51:34 +0530

    Update AddStudent.java

--- queriesgen/AddStudent.java
+++ queriesgen/AddStudent.java
@@ -6,7 +6,6 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
