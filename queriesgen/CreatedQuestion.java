d62777a9b0674477fc06ca79ca99d0374d3537c8
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-01 09:52:56 +0530

    Update CreatedQuestion.java

--- queriesgen/CreatedQuestion.java
+++ queriesgen/CreatedQuestion.java
@@ -7,7 +7,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
-
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
