05cde702ffc6f286069ea5ef7d186300153c4de7
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-01 09:46:12 +0530

    Update AdminDashboard.java

--- queriesgen/AdminDashboard.java
+++ queriesgen/AdminDashboard.java
@@ -17,8 +17,7 @@ public class AdminDashboard extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin_dashboard);
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bill), (v, insets) -> {
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bill), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
