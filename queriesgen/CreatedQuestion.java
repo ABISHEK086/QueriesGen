4a84a1ee3941ef0e75b8d6f8d736e3c748e3b6b3
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-01 09:54:41 +0530

    Update CreatedQuestion.java

--- queriesgen/CreatedQuestion.java
+++ queriesgen/CreatedQuestion.java
@@ -21,7 +21,7 @@ public class CreatedQuestion extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_created_question);
-  subjectText = findViewById(R.id.subject_text);
+        subjectText = findViewById(R.id.subject_text);
         difficultyText = findViewById(R.id.difficulty_text);
         pdfOpenText = findViewById(R.id.pdf_open_text); // Clickable TextView to open PDF
 
