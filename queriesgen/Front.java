a5cc1b32e6294ca307f1f7449248eaaa0ef130e2
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-02 08:57:30 +0530

    Update Front.java

--- queriesgen/Front.java
+++ queriesgen/Front.java
@@ -13,7 +13,6 @@ public class Front extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
-
 Button getStartedButton = findViewById(R.id.bottom_button);
 
 
