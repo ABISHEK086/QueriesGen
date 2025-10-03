f3ca00a6f785d81d740a22b7a95019041385af11
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-03 09:45:33 +0530

    Update Generate.java

--- queriesgen/Generate.java
+++ queriesgen/Generate.java
@@ -51,7 +51,7 @@ public class Generate extends AppCompatActivity {
             }
         });
 
-        // Open PDF Button Click
+
         openPdfButton.setOnClickListener(view -> openPDF());
     }
 
