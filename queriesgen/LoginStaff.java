8ae61e454af893b8bd34827e88aaf034fd69e3ac
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-12 12:04:43 +0530

    Update LoginStaff.java

--- queriesgen/LoginStaff.java
+++ queriesgen/LoginStaff.java
@@ -23,7 +23,7 @@ public class LoginStaff extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_staff);
 
-        // Initiali
+        // Initial
         mAuth = FirebaseAuth.getInstance();
 
         // Initialize Views
