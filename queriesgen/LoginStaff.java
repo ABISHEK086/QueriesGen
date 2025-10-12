91630dd0d321efd3f5d1427fc56a55b527d4cd8a
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-12 12:01:14 +0530

    Update LoginStaff.java

--- queriesgen/LoginStaff.java
+++ queriesgen/LoginStaff.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
