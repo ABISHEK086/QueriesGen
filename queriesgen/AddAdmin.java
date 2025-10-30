3c9283f83047ad97df49450a83fc7762760f3e17
ABISHEK ANTONY <abishek8673@gmail.com>
2025-10-30 17:17:01 +0530

    Update AddAdmin.java

--- queriesgen/AddAdmin.java
+++ queriesgen/AddAdmin.java
@@ -1,7 +1,7 @@
 package com.example.queriesgen;        
   
 import android.os.Bundle;
-import android.view.View; 
+import android.view.View;  
 import android.widget.Button;
 import android.widget.EditText; 
 import android.widget.ProgressBar; 
