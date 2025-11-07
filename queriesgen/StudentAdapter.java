2207da85538e022ef80aaae3a33f0d79ae64d449
ABISHEK ANTONY <abishek8673@gmail.com>
2025-11-07 21:27:32 +0530

    Update StudentAdapter.java

--- queriesgen/StudentAdapter.java
+++ queriesgen/StudentAdapter.java
@@ -5,7 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;    
 import android.view.ViewGroup;   
 import android.widget.ImageView;  
-import android.widget.TextView;   
+import android.widget.TextView;    
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -81,6 +81,7 @@ public class StudentAdapter extends RecyclerView.Adapter<StudentAdapter.ViewHold
         }
     }
 }
+
 
 
 
