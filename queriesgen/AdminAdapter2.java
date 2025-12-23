81391a80c5bee6fc7c7dfd27ecc0ce1e5c65727f
ABISHEK ANTONY <abishek8673@gmail.com>
2025-12-23 18:23:08 +0530

    Update AdminAdapter2.java

--- queriesgen/AdminAdapter2.java
+++ queriesgen/AdminAdapter2.java
@@ -7,7 +7,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;      
 import androidx.recyclerview.widget.RecyclerView;   
 import java.util.List;
-
+ 
 public class AdminAdapter2 extends RecyclerView.Adapter<AdminAdapter2.ViewHolder> {
 
     private List<Admin> adminList;
@@ -45,6 +45,7 @@ public class AdminAdapter2 extends RecyclerView.Adapter<AdminAdapter2.ViewHolder
         }
     }
 }
+
 
 
 
