d2ca11710c197bab012092f30e187c8f64f044cd
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-05 17:22:03 +0530

    Fix formatting in CreatedQuestion.java imports

--- queriesgen/CreatedQuestion.java
+++ queriesgen/CreatedQuestion.java
@@ -3,7 +3,7 @@ package com.example.queriesgen;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.TextView; 
 import android.widget.Toast; 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
