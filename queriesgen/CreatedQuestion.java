7b7aa7ae39e768d291b9f22c64fd431487725fe3
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-05 17:24:26 +0530

    Fix import formatting in CreatedQuestion.java

--- queriesgen/CreatedQuestion.java
+++ queriesgen/CreatedQuestion.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.net.Uri; 
 import android.os.Bundle;
 import android.widget.TextView; 
-import android.widget.Toast;  
+import android.widget.Toast;   
 import androidx.appcompat.app.AppCompatActivity;   
 import androidx.core.content.FileProvider; 
 import java.io.File;
