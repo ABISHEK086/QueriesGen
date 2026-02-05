e8d70fbea04b231cef8202709cf9934dee5a0f14
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-05 17:27:22 +0530

    Update StudentDashboard.java

--- queriesgen/StudentDashboard.java
+++ queriesgen/StudentDashboard.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;  
 import androidx.activity.EdgeToEdge; 
-import androidx.appcompat.app.AppCompatActivity;  
+import androidx.appcompat.app.AppCompatActivity;   
 import androidx.cardview.widget.CardView;
  
 public class StudentDashboard extends AppCompatActivity {
