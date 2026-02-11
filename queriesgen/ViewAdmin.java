0d0ea64ce25614e951e94a4d4d9db135fcd6f26a
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-11 09:46:08 +0530

    Update ViewAdmin.java

--- queriesgen/ViewAdmin.java
+++ queriesgen/ViewAdmin.java
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseError; 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
