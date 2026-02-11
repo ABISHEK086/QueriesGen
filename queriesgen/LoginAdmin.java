4be7263ac3fbf192245a836c9cf295d495ca6f9a
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-11 09:44:06 +0530

    Update LoginAdmin.java

--- queriesgen/LoginAdmin.java
+++ queriesgen/LoginAdmin.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.widget.Button;    
 import android.widget.EditText;         
 import android.widget.ProgressBar;           
-import android.widget.Toast;     
+import android.widget.Toast;      
   
 import androidx.appcompat.app.AppCompatActivity;       
  
