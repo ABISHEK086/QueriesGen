e7bb453e5c4a4734474f6cf94e6aa81a8b3be1e0
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-11 09:37:35 +0530

    Update LoginAdmin.java

--- queriesgen/LoginAdmin.java
+++ queriesgen/LoginAdmin.java
@@ -10,7 +10,7 @@ import android.widget.ProgressBar;
 import android.widget.Toast;   
 
 import androidx.appcompat.app.AppCompatActivity;  
-
+ 
 import com.google.firebase.auth.FirebaseAuth; 
 import com.google.firebase.auth.FirebaseUser;
 
