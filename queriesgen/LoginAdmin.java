93d140342b900a6f9c62832838fc163634e3cf42
ABISHEK ANTONY <abishek8673@gmail.com>
2026-02-11 09:35:42 +0530

    Update LoginAdmin.java

--- queriesgen/LoginAdmin.java
+++ queriesgen/LoginAdmin.java
@@ -3,7 +3,7 @@ package com.example.queriesgen;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.view.View; 
 import android.widget.Button; 
 import android.widget.EditText;    
 import android.widget.ProgressBar; 
