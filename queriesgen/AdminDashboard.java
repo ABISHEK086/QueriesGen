ddf70c5a153a695b6dd86c2895fd7e140b75eec4
ABISHEK ANTONY <abishek8673@gmail.com>
2026-03-09 15:50:47 +0530

    Update AdminDashboard.java

--- queriesgen/AdminDashboard.java
+++ queriesgen/AdminDashboard.java
@@ -10,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat; 
 import androidx.core.view.WindowInsetsCompat; 
  
-public class AdminDashboard extends AppCompatActivity {
+public class AdminDashboard extends AppCompatActivity { 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
