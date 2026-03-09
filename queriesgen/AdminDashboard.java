b7d7c33355bef27fe48c268bad4a781fd64e9a51
ABISHEK ANTONY <abishek8673@gmail.com>
2026-03-09 15:50:01 +0530

    Update AdminDashboard.java

--- queriesgen/AdminDashboard.java
+++ queriesgen/AdminDashboard.java
@@ -4,7 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import androidx.activity.EdgeToEdge;  
-import androidx.appcompat.app.AppCompatActivity; 
+import androidx.appcompat.app.AppCompatActivity;   
 import androidx.cardview.widget.CardView; 
 import androidx.core.graphics.Insets; 
 import androidx.core.view.ViewCompat; 
